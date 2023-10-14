6ee8a5cf3ee47ee95c41fcb28b2028c1188611f6
vankha1 <vovankha2003@gmail.com>
2023-10-14 16:03:26 +0700

    [dev]: align category and being responsive course site

--- FrontEnd/src/components/common/Card/CardCourse.tsx
+++ FrontEnd/src/components/common/Card/CardCourse.tsx
@@ -9,8 +9,8 @@ interface CardCourseProps extends React.ComponentProps<'div'> {
 export default function CardCourse(props: CardCourseProps){
 
     return (
-        <div className={`flex mb-4 ${props.className}`}>
-            <img className="w-52 mr-8" src="https://ksr-ugc.imgix.net/assets/025/867/455/c9e6c72592a0c2daff268afb45d604d7_original.png?ixlib=rb-4.1.0&crop=faces&w=1552&h=873&fit=crop&v=1563593007&auto=format&frame=1&q=92&s=035154de01cf39f862fe61fb730735dd" alt="logoCourse" />
+        <div className={`md:flex mb-16 px-4 md:mb-4 ${props.className}`}>
+            <img className="md:shrink-0 md:h-full md:w-48 w-full object-cover mb-2 mr-8" src="https://ksr-ugc.imgix.net/assets/025/867/455/c9e6c72592a0c2daff268afb45d604d7_original.png?ixlib=rb-4.1.0&crop=faces&w=1552&h=873&fit=crop&v=1563593007&auto=format&frame=1&q=92&s=035154de01cf39f862fe61fb730735dd" alt="logoCourse" />
             <div className="card-content basis-1/2">
                 <h2 className="mb-2 bg-stone-600 p-2">{props.title}</h2>
                 <p>{props.numberOf} Nguoi hoc | Tao boi {props.author}</p>
